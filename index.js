9f341b0b45a8311fa67f306433fcba6830c99700
cjlora <cjlora@gmail.com>
2022-07-14 18:08:34 +0800

    Updated nodes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { joinVoiceChannel } = require("@discordjs/voice");
 const { addSpeechEvent } = require("discord-speech-recognition");
 
 require('dotenv').config();
+require('events').EventEmitter.prototype._maxListeners = 100;
 
 const client = new Client({
   intents: [
@@ -26,10 +27,29 @@ client.on("messageCreate", (msg) => {
 });
 
 client.on("speech", (msg) => {
-  // If bot didn't recognize speech, content will be empty
-  if (!msg.content) return;
+  if(msg.author.id == "468467136483033091"){
+    client.channels.cache.get(`995556648846381128`).send(msg.content);
 
-  msg.author.send(msg.content);
+    if(msg.content == "silence"){
+      let channel = msg.guild.channels.cache.get(msg.member.voice.channel.id);
+      for (const member of channel.members.values()){
+        if(member.id == "468467136483033091" || member.id == "995686536681095350")
+          continue;
+        member.voice.setMute(true);
+        member.voice.setDeaf(true);
+      }
+    }
+
+    if(msg.content == "hear me"){
+      let channel = msg.guild.channels.cache.get(msg.member.voice.channel.id);
+      for (const member of channel.members.values()){
+        if(member.id == "468467136483033091" || member.id == "995686536681095350")
+          continue;
+        member.voice.setMute(false);
+        member.voice.setDeaf(false);
+      }
+    }
+  }
 });
 
 client.on("ready", () => {
